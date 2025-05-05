4b10b2b7220415659a08413034a6c3ffce5053d9
SimoneZampieri <simonezampieri106@gmail.com>
2025-05-05 12:34:49 +0200

    milestone 2 completed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 type Person = {
-    id: number;
-    name: string;
+    readonly id: number;
+    readonly name: string;
     birth_year: number;
-    death_year: number;
+    death_year?: number;
     biography: string;
     image: string;
+}
+
+//varie nazionalità
+type Nationality = 
+  | "American" 
+  | "British" 
+  | "Australian" 
+  | "Israeli-American" 
+  | "South African" 
+  | "French" 
+  | "Indian" 
+  | "Israeli" 
+  | "Spanish" 
+  | "South Korean" 
+  | "Chinese";
+
+
+type Actress = Person & {
+    most_famous_movie: [string, string, string];
+    awards: string;
+    nationality: Nationality;
 }
